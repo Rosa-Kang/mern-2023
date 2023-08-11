5d240c8528e2d219044e54f671567c5be9a1bd55
Rosa Kang <therosessom@gmail.com>
2023-08-11 13:41:40 -0700

    organize/nest files for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
-import { Box, CssBaseline } from "@mui/material";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { useMemo } from "react"
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 function App() {
-  // const theme = useMemo(() => createTheme(themeSettings));
-  
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div className='app'>
+    <div className="app">
       <BrowserRouter>
-      <ThemeProvider >
+        <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box width="100%" height="100%" padding="1rem 2rem 4rem" color="white">
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard Page</div>} />
-              <Route path="/prediction" element={<div>Prediction Page</div>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </Box>
-      </ThemeProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
